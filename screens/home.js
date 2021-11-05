311a094800b957987aa2efcf47ae220d64d835f3
jmw75 <jmw75@maroon20.cs.calvin.edu>
2021-11-05 01:36:24 -0400

    additional events

--- screens/home.js
+++ screens/home.js
@@ -4,7 +4,15 @@ import { globalStyles } from '../styles/global';
 
 export default function HomeScreen({ navigation }) {
 
-  const test_image = { uri: "https://images.unsplash.com/photo-1551698618-1dfe5d97d256?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2tpfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" };
+  const test_image = { uri: "https://coresites-cdn-adm.imgix.net/mpora_new/wp-content/uploads/2016/07/SkiWords.jpg?fit=crop" };
+
+  const meijer_image = { uri: "https://www.supermarketnews.com/sites/supermarketnews.com/files/styles/article_featured_retina/public/Meijer%20supercenter%20exterior.jpg?itok=GGkiO9BI" };
+  const rockClimbing_image = { uri: "https://media.self.com/photos/5d8a5f472aa2920009962a7e/4:3/w_2560%2Cc_limit/GettyImages-699099379.jpg" };
+  const movie_image = { uri: "https://s.wsj.net/public/resources/images/ON-CE927_moviet_B1280_20170714200426.jpg" };
+  const beach_image = { uri: "https://res.cloudinary.com/miles-extranet-dev/image/upload/ar_16:9,c_fill,w_1000,g_face,q_50/Michigan/account_photos/7003/a3d989434d77bb24952c5960b2e30154_BeachScene.jpg" };
+  const skydive_image = { uri: "https://www.insideedition.com/sites/default/files/styles/video_1920x1080/public/images/2018-11/112818-sjydiving2-1280x720.jpg?h=c673cd1c&width=1170&height=658" };
+  
+
   return (
     <View style={globalStyles.homepage}>
       <View style={globalStyles.eventsViewEventsContainer}>
@@ -14,8 +22,47 @@ export default function HomeScreen({ navigation }) {
             <Text style={globalStyles.textInsideContainer}>Skiing</Text>
           </ImageBackground>
         </TouchableOpacity>
-      </View>
+ 
+        <TouchableOpacity
+          onPress={() => navigation.navigate('View Event', {})}>
+          <ImageBackground source={movie_image} style={globalStyles.eventsListImageBackground}>
+            <Text style={globalStyles.textInsideContainer}>Movie</Text>
+          </ImageBackground>
+        </TouchableOpacity>
 
+        <TouchableOpacity
+          onPress={() => navigation.navigate('View Event', {})}>
+          <ImageBackground source={rockClimbing_image} style={globalStyles.eventsListImageBackground}>
+            <Text style={globalStyles.textInsideContainer}>Rock Climbing</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('View Event', {})}>
+          <ImageBackground source={meijer_image} style={globalStyles.eventsListImageBackground}>
+            <Text style={globalStyles.textInsideContainer}>Grocery Store</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('View Event', {})}>
+          <ImageBackground source={beach_image} style={globalStyles.eventsListImageBackground}>
+            <Text style={globalStyles.textInsideContainer}>Beach</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        
+        <TouchableOpacity
+          onPress={() => navigation.navigate('View Event', {})}>
+          <ImageBackground source={skydive_image} style={globalStyles.eventsListImageBackground}>
+            <Text style={globalStyles.textInsideContainer}>Sky Diving</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+
+
+      </View>
+    
 
       <View style={globalStyles.wrapper}>
         <View style={globalStyles.buttonv}>
