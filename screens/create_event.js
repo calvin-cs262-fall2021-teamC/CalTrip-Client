e12dcae1659f9ae05390923fbb92dcbd61a60c2f
Kun Kang <kunkang@Kuns-MacBook-Pro.local>
2021-11-11 23:40:14 -0500

    connect and retrieve from database

--- screens/create_event.js
+++ screens/create_event.js
@@ -4,71 +4,73 @@ import { globalStyles } from '../styles/global';
 
 export default function CreateEvent({ navigation }) {
   return (
-    <View style={globalStyles.contentContainer}>
-      <View style={globalStyles.createEventContentContainer}>
+    <ScrollView>
+      <View style={globalStyles.contentContainer}>
+        <View style={globalStyles.creteEventContentContainer}>
 
-        <View style={globalStyles.createEventInputBox}>
-          <Text style={globalStyles.subTitle}>Title</Text>
-          <TextInput style={globalStyles.inputBox}
-            placeholder="Type title of the event..."
+          <View style={globalStyles.createEventInputBox}>
+            <Text style={globalStyles.subTitle}>Title</Text>
+            <TextInput style={globalStyles.inputBox}
+              placeholder="Type title of the event..."
             />
-        </View>
+          </View>
 
-        <View style={globalStyles.createEventInputBox}>
-          <Text style={globalStyles.subTitle}>Address</Text>
-          <TextInput style={globalStyles.inputBox}
-            placeholder="Type address of the event..."
+          <View style={globalStyles.createEventInputBox}>
+            <Text style={globalStyles.subTitle}>Address</Text>
+            <TextInput style={globalStyles.inputBox}
+              placeholder="Type address of the event..."
+            />
+          </View>
+
+          <View style={globalStyles.createEventInputBox}>
+            <Text style={globalStyles.subTitle}>Start date</Text>
+            <TextInput style={globalStyles.inputBox}
+              placeholder="(mm/dd/yyyy)"
+            />
+          </View>
+
+          <View style={globalStyles.createEventInputBox}>
+            <Text style={globalStyles.subTitle}>End date</Text>
+            <TextInput style={globalStyles.inputBox}
+              placeholder="(mm/dd/yyyy)"
+            />
+          </View>
+
+          <View style={globalStyles.createEventInputBox}>
+            <Text style={globalStyles.subTitle}>Price</Text>
+            <TextInput style={globalStyles.inputBox}
+              placeholder="Type price of the event..."
+            />
+          </View>
+
+          <View style={globalStyles.createEventInputBox}>
+            <Text style={globalStyles.subTitle}>Description</Text>
+            <TextInput style={globalStyles.inputBox}
+              placeholder="Introduce the details of the event..."
+            />
+          </View>
+
+        </View>
+        <View style={globalStyles.bottomWrapper}>
+          <Button
+            marginBottom='5%'
+            color='#75022c'
+            title="Create Event"
+            onPress={() => {
+              Alert.alert(
+                "Congratulations!",
+                "You successfully created the trip.",
+                [
+                  {
+                    text: "OK",
+                    onPress: () => { navigation.navigate('Home'); }
+                  }
+                ]
+              );
+            }}
           />
         </View>
-
-        <View style={globalStyles.createEventInputBox}>
-          <Text style={globalStyles.subTitle}>Start date</Text>
-          <TextInput style={globalStyles.inputBox}
-            placeholder="(mm/dd/yyyy)"
-          />
-        </View>
-
-        <View style={globalStyles.createEventInputBox}>
-          <Text style={globalStyles.subTitle}>End date</Text>
-          <TextInput style={globalStyles.inputBox}
-            placeholder="(mm/dd/yyyy)"
-          />
-        </View>
-
-        <View style={globalStyles.createEventInputBox}>
-          <Text style={globalStyles.subTitle}>Price</Text>
-          <TextInput style={globalStyles.inputBox}
-            placeholder="Type price of the event..."
-          />
-        </View>
-
-        <View style={globalStyles.createEventInputBox}>
-          <Text style={globalStyles.subTitle}>Description</Text>
-          <TextInput style={globalStyles.inputBox}
-            placeholder="Introduce the details of the event..."
-          />
-        </View>
-
       </View>
-      <View style={globalStyles.bottomWrapper}>
-      <Button
-        marginBottom='5%'
-        color='#75022c'
-        title="Create Event"
-        onPress={() => {
-            Alert.alert(
-              "Congratulations!",
-              "You successfully created the trip.",
-              [
-                {
-                  text: "OK",
-                  onPress: () => { navigation.navigate('Home'); }
-                }
-              ]
-            );
-        }}
-      />
-      </View>
-    </View>
+    </ScrollView>
   );
 }
--- screens/home.js
+++ screens/home.js
@@ -1,68 +1,41 @@
-import React, { useState } from 'react';
-import { View, Text, Button, TouchableOpacity, ImageBackground, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, View, Text, Button, TouchableOpacity, ImageBackground, ScrollView } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function HomeScreen({ navigation }) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://caltrip-service.herokuapp.com/events`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   const test_image = { uri: "https://coresites-cdn-adm.imgix.net/mpora_new/wp-content/uploads/2016/07/SkiWords.jpg?fit=crop" };
 
-  const meijer_image = { uri: "https://www.supermarketnews.com/sites/supermarketnews.com/files/styles/article_featured_retina/public/Meijer%20supercenter%20exterior.jpg?itok=GGkiO9BI" };
-  const rockClimbing_image = { uri: "https://media.self.com/photos/5d8a5f472aa2920009962a7e/4:3/w_2560%2Cc_limit/GettyImages-699099379.jpg" };
-  const movie_image = { uri: "https://s.wsj.net/public/resources/images/ON-CE927_moviet_B1280_20170714200426.jpg" };
-  const beach_image = { uri: "https://res.cloudinary.com/miles-extranet-dev/image/upload/ar_16:9,c_fill,w_1000,g_face,q_50/Michigan/account_photos/7003/a3d989434d77bb24952c5960b2e30154_BeachScene.jpg" };
-  const skydive_image = { uri: "https://www.insideedition.com/sites/default/files/styles/video_1920x1080/public/images/2018-11/112818-sjydiving2-1280x720.jpg?h=c673cd1c&width=1170&height=658" };
-  
-
   return (
     <ScrollView>
-    <View style={globalStyles.homepage}>
-      <View style={globalStyles.eventsViewEventsContainer}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('View Event', {})}>
-          <ImageBackground source={test_image} style={globalStyles.eventsListImageBackground}>
-            <Text style={globalStyles.textInsideContainer}>Skiing</Text>
-          </ImageBackground>
-        </TouchableOpacity>
- 
-        <TouchableOpacity
-          onPress={() => navigation.navigate('View Event', {})}>
-          <ImageBackground source={movie_image} style={globalStyles.eventsListImageBackground}>
-            <Text style={globalStyles.textInsideContainer}>Movie</Text>
-          </ImageBackground>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate('View Event', {})}>
-          <ImageBackground source={rockClimbing_image} style={globalStyles.eventsListImageBackground}>
-            <Text style={globalStyles.textInsideContainer}>Rock Climbing</Text>
-          </ImageBackground>
-        </TouchableOpacity>
-
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate('View Event', {})}>
-          <ImageBackground source={meijer_image} style={globalStyles.eventsListImageBackground}>
-            <Text style={globalStyles.textInsideContainer}>Grocery Store</Text>
-          </ImageBackground>
-        </TouchableOpacity>
-
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate('View Event', {})}>
-          <ImageBackground source={skydive_image} style={globalStyles.eventsListImageBackground}>
-            <Text style={globalStyles.textInsideContainer}>Sky Diving</Text>
-          </ImageBackground>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => navigation.navigate('View Event', {})}>
-          <ImageBackground source={beach_image} style={globalStyles.eventsListImageBackground}>
-            <Text style={globalStyles.textInsideContainer}>Beach</Text>
-          </ImageBackground>
-        </TouchableOpacity>
+      <View style={globalStyles.homepage}>
+        {isLoading ? <ActivityIndicator /> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate('View Event', item)}>
+                <ImageBackground source={test_image} style={globalStyles.eventsListImageBackground}>
+                  <Text style={globalStyles.textInsideContainer}>{item.name}</Text>
+                </ImageBackground>
+              </TouchableOpacity>
+            )}
+          />
+        )}
 
       </View>
-    
+
       <View style={globalStyles.wrapper}>
         <View style={globalStyles.buttonv}>
           <Button
@@ -80,7 +53,6 @@ export default function HomeScreen({ navigation }) {
           <Text style={globalStyles.plus}>+</Text>
         </TouchableOpacity>
       </View>
-    </View>
-    </ScrollView>
+    </ScrollView >
   );
 }
