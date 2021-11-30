1af95827db539be2f2041659638bb5cfd5ac4e0e
YkSlavik <81549171+YkSlavik@users.noreply.github.com>
2021-11-29 21:40:09 -0500

    CreateAccount page modifications

--- screens/signup.js
+++ screens/signup.js
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { View, Text, TextInput, Button, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import { signupStyles } from '../styles/signup_style';
 
 export default function SignupScreen({ navigation }) {
+  const [inputFirstName, setFirstName] = useState('');
+  const [inputLastName, setLastName] = useState('');
+  const [inputEmail, setEmail] = useState('');
+  const [inputPassword, setPassword] = useState('');
+
+  const sendData = async () => {
+    try {
+      const response = await fetch("https://caltrip-service.herokuapp.com/users", {
+        method: "POST",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: inputFirstName,
+          lastName: inputLastName,
+          email: inputEmail,
+          password: inputPassword,
+        })
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <View style={signupStyles.contentContainer}>
+    <View style={signupStyles.scrollViewHolder}>
     <ScrollView>
-
-      <View style={signupStyles.createEventContentContainer}>
 
         <View style={signupStyles.createEventInputBox}>
           <Text style={signupStyles.subTitle}>First name</Text>
           <TextInput style={signupStyles.inputBox}
+            onChangeText={text => setFirstName(text)}
+            value={inputFirstName}
             />
         </View>
 
         <View style={signupStyles.createEventInputBox}>
           <Text style={signupStyles.subTitle}>Last Name</Text>
           <TextInput style={signupStyles.inputBox}
-        />
+            onChangeText={text => setFirstName(text)}
+            value={inputLastName}
+            />
         </View>
 
         <View style={signupStyles.createEventInputBox}>
           <Text style={signupStyles.subTitle}>Email</Text>
           <TextInput style={signupStyles.inputBox}
+            onChangeText={text => setFirstName(text)}
+            value={inputEmail}
             />
         </View>
 
         <View style={signupStyles.createEventInputBox}>
           <Text style={signupStyles.subTitle}>Password</Text>
           <TextInput secureTextEntry={true} style={signupStyles.inputBox}
+            onChangeText={text => setFirstName(text)}
+            value={inputPassword}
             />
         </View>
 
@@ -39,8 +72,8 @@ export default function SignupScreen({ navigation }) {
             />
         </View>
 
-      </View>
-      </ScrollView>
+    </ScrollView>
+    </View>
 
     <View style={signupStyles.bottomWrapper}>
       <Button
