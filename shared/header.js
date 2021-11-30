e3a35e062aaa4cf5a0bfab2b101377851d88f050
YkSlavik <81549171+YkSlavik@users.noreply.github.com>
2021-11-29 22:58:04 -0500

    Changed size of the logo

--- shared/header.js
+++ shared/header.js
@@ -9,7 +9,7 @@ export default function Header({ navigation }) {
         <View>
             <TouchableOpacity onPress={() => navigation.navigate('My Account')}>
                 <Image
-                  style={{ width: 40, height: 40, borderRadius: 40, marginBottom:20}}
+                  style={{ width: 30, height: 30, borderRadius: 30}}
                   source={{
                   uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-udL_-Me4EZSeIPL_RFegnzJ6a9WKGLP2YQ&usqp=CAU',
                         }}
