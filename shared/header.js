0154b7cdbb6bf1ff313515eccf37dfe6033f97c5
samuelth47 <samtad4774@gmail.com>
2021-11-29 22:08:03 -0500

    Update from 'My Account' to a logo

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { TouchableOpacity, View, Text, Image } from 'react-native';
 
 import { globalStyles } from '../styles/global';
 
@@ -8,8 +8,13 @@ export default function Header({ navigation }) {
     return (
         <View>
             <TouchableOpacity onPress={() => navigation.navigate('My Account')}>
-                <Text style={globalStyles.default}>My Account</Text>
+                <Image
+                  style={{ width: 40, height: 40, borderRadius: 40, marginBottom:20}}
+                  source={{
+                  uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-udL_-Me4EZSeIPL_RFegnzJ6a9WKGLP2YQ&usqp=CAU',
+                        }}
+                />
             </TouchableOpacity>
         </View>
     );
-};
+}
