4da00023eb27ae97ae6b279911f4e525849c9ee1
samuelth47 <samtad4774@gmail.com>
2021-12-10 00:07:15 -0500

    Added the help page and a header for it

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ import { globalStyles } from './styles/global';
 import HomeScreen from "./screens/home";
 import LoginScreen from "./screens/login";
 import MonthsScreen from "./screens/months";
+import HelpScreen from "./screens/helpPage";
 import Header from "./shared/header";
+import HelpShared from "./shared/help";
 import Account from './screens/account';
 import CreateEvent from './screens/create_event';
 import JoinEvent from './screens/join_event';
@@ -34,7 +36,12 @@ export default function App() {
     <NavigationContainer>
       {/* The landing page will be the Login page */}
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Overview' }, ({ navigation }) => ({
+          headerRight: () => (
+            <Header navigation={navigation} />
+          )
+        })
+        } />
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }, ({ navigation }) => ({
           headerRight: () => (
             <Header navigation={navigation} />
@@ -42,9 +49,19 @@ export default function App() {
         })
         } />
 
-        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ title: 'Overview' }, ({ navigation }) => ({
+          headerRight: () => (
+            <HelpShared navigation={navigation} />
+          )
+        })
+        } />
 
-        <Stack.Screen name="My Account" component={Account} />
+        <Stack.Screen name="My Account" component={Account} options={({ navigation }) => ({
+          headerRight: () => (
+            <HelpShared navigation={navigation} />
+          )
+        })
+        } />
 
         <Stack.Screen name="Create Event" component={CreateEvent} options={{ title: 'Overview' }, ({ navigation }) => ({
           headerRight: () => (
@@ -52,7 +69,12 @@ export default function App() {
           )
         })
         } />
-        <Stack.Screen name="View Event" component={ViewEvent} />
+        <Stack.Screen name="View Event" component={ViewEvent} options={{ title: 'Overview' }, ({ navigation }) => ({
+          headerRight: () => (
+            <Header navigation={navigation} />
+          )
+        })
+        } />
         <Stack.Screen name="Join Event" component={JoinEvent} options={{ title: 'Overview' }, ({ navigation }) => ({
           headerRight: () => (
             <Header navigation={navigation} />
@@ -66,7 +88,8 @@ export default function App() {
           )
         })
         } />
-        
+
+        <Stack.Screen name="Help Screen" component={HelpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
