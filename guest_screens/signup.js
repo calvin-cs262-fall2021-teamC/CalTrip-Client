a263dbe13cd7c6a3df503a8d0859be27fd2d3581
YkSlavik <81549171+YkSlavik@users.noreply.github.com>
2021-12-11 04:35:06 -0500

    created guest screens

---
+++ guest_screens/signup.js
@@ -0,0 +1,100 @@
+import React, { useState} from 'react';
+import { View, Text, TextInput, Button, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import { signupStyles } from '../styles/signup_style';
+
+export default function GuestSignupScreen({ navigation }) {
+  const [inputFirstName, setFirstName] = useState('');
+  const [inputLastName, setLastName] = useState('');
+  const [inputEmailAddress, setEmailAddress] = useState('');
+  const [inputPassword, setPassword] = useState('');
+
+  const sendData = async () => {
+    try {
+      const response = await fetch("https://caltrip-service.herokuapp.com/users", {
+        method: "POST",
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: inputFirstName,
+          lastName: inputLastName,
+          emailAddress: inputEmailAddress,
+          password: inputPassword
+        })
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <View style={signupStyles.contentContainer}>
+    <View style={signupStyles.scrollViewHolder}>
+    <ScrollView>
+
+        <View style={signupStyles.createEventInputBox}>
+          <Text style={signupStyles.subTitle}>First name</Text>
+          <TextInput style={signupStyles.inputBox}
+            onChangeText={text => setFirstName(text)}
+            value={inputFirstName}
+            />
+        </View>
+
+        <View style={signupStyles.createEventInputBox}>
+          <Text style={signupStyles.subTitle}>Last Name</Text>
+          <TextInput style={signupStyles.inputBox}
+            onChangeText={text => setLastName(text)}
+            value={inputLastName}
+            />
+        </View>
+
+        <View style={signupStyles.createEventInputBox}>
+          <Text style={signupStyles.subTitle}>Email</Text>
+          <TextInput style={signupStyles.inputBox}
+            onChangeText={text => setEmailAddress(text)}
+            value={inputEmailAddress}
+            />
+        </View>
+
+        <View style={signupStyles.createEventInputBox}>
+          <Text style={signupStyles.subTitle}>Password</Text>
+          <TextInput secureTextEntry={true} style={signupStyles.inputBox}
+            onChangeText={text => setPassword(text)}
+            value={inputPassword}
+            />
+        </View>
+
+        <View style={signupStyles.createEventInputBox}>
+          <Text style={signupStyles.subTitle}>Repeat Password</Text>
+          <TextInput secureTextEntry={true} style={signupStyles.inputBox}
+            />
+        </View>
+
+    </ScrollView>
+    </View>
+
+    <View style={signupStyles.bottomWrapper}>
+      <Button
+        color='#75022c'
+        title="Create account"
+        onPress={() => {
+          sendData();
+            Alert.alert(
+              "Congratulations!",
+              "You successfully created an account.",
+              [
+                {
+                  text: "OK",
+                  onPress: () => { navigation.navigate('Login'); }
+                }
+              ]
+            );
+          }}
+          />
+      </View>
+    </View>
+
+  );
+}
---
+++ guest_screens/view_event.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, TextInput, Button, TouchableOpacity, Image } from 'react-native';
+import { eventStyles } from '../styles/event_style';
+import images from '../imageEvent/images'
+
+export default function GuestViewEvent({ route, navigation }) {
+  return (
+    <View style={eventStyles.contentContainer}>
+      <View style={eventStyles.default}>
+        <Image
+          style={{ width: "100%", height: 200 }}
+          source={images[route.params.category]}
+        />
+        <Text style={{ fontWeight: "bold", padding: 15, fontSize: 24 }}> {route.params.title}</Text>
+
+        <View style={eventStyles.eventViewTextContainer}>
+          <Text style={eventStyles.eventViewEventDescription}>{route.params.description}</Text>
+          <Text style={eventStyles.eventViewEventDescription}><Text style={{ fontWeight: "bold" }}>Start Date:</Text> {route.params.startdate}</Text>
+          <Text style={eventStyles.eventViewEventDescription}><Text style={{ fontWeight: "bold" }}>End Date:</Text>{route.params.enddate}</Text>
+          <Text style={eventStyles.eventViewEventDescription}><Text style={{ fontWeight: "bold" }}>Location:</Text> {route.params.location} </Text>
+          <Text style={eventStyles.eventViewEventDescription}><Text style={{ fontWeight: "bold" }}>Price: $</Text> {route.params.price} </Text>
+        </View>
+
+      </View>
+
+    </View>
+
+  );
+}
